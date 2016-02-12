645b4ecb8fd038e64fa47e586e64d5f39a5b1824
TekTutorialsHub <admin@TekTutorialsHub.com>
2016-02-12 20:56:45 +0530

    Created

---
+++ EFFluentAPIModelMapping/EFContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EFFluentAPIModelMapping
+{
+    public class EFContext : DbContext
+    {
+        public EFContext()
+            : base("EFDatabase")
+        {
+            Database.SetInitializer<EFContext>(new DropCreateDatabaseAlways<EFContext>());
+        }
+
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.HasDefaultSchema("Admin");
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ EFFluentAPIModelMapping/models.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFFluentAPIModelMapping
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+    }
+}
